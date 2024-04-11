bbd540dad674b565082a4227b87386e09c05a2e4
gs0428 <lider28@naver.com>
2024-04-11 18:36:20 +0900

    feat: rollup으로 react 실행시켜보기

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,15 +1,37 @@
-// rollup.config.js
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import replace from "@rollup/plugin-replace";
 
 export default {
-  input: "src/index.js",
+  input: "./src/index.jsx",
   output: {
-    file: "dist/bundle.js",
+    file: "./dist/bundle.js",
     format: "iife",
   },
   plugins: [
-    resolve(), // Node.js 모듈을 해석합니다.
-    commonjs(), // CommonJS 모듈을 ES6으로 변환합니다.
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-env"],
+    }),
+    ...(process.env.ROLLUP_WATCH
+      ? [
+          serve({
+            open: true, // 브라우저에서 바로 열기
+            contentBase: ["dist"], // 정적 파일 제공 디렉토리
+            host: "localhost",
+            port: 10001,
+          }),
+          livereload("dist"), // livereload 대상 디렉토리
+        ]
+      : []),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: true,
+    }),
   ],
 };
